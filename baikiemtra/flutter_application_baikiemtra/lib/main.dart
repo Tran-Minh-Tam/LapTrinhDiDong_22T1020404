dbd6a9823f4c04773b372c515eaf9da3daab719b
Tran Minh Tam <22T1020404@husc.edu.vn>
2025-12-11 16:51:24 +0700

    Tam: Chinh sua tieu de

--- baikiemtra/flutter_application_baikiemtra/lib/main.dart
+++ baikiemtra/flutter_application_baikiemtra/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Bài Tập Lớn Flutter',
+      title: 'Bài Kiểm Tra LTDĐ Nhóm 6',
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       home: const MainScreen(),
     );
